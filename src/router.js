0260007f216722c6b9a0d42b432a702e6da5c761
DC Tu <66411244+DCTuMusic@users.noreply.github.com>
2020-07-31 01:02:13 +0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -65,14 +65,14 @@ export default new Router({
 
 
 
-export default router;
+// export default router;
 
-router.beforeEach((to, from, next) => {
-    // Redirect if fullPath begins with a hash (ignore hashes later in path)
-    if (to.fullPath.substr(0,2) === "/#") {
-        const path = to.fullPath.substr(2);
-        next(path);
-        return;
-    }
-    next();
-});
+// router.beforeEach((to, from, next) => {
+//     // Redirect if fullPath begins with a hash (ignore hashes later in path)
+//     if (to.fullPath.substr(0,2) === "/#") {
+//         const path = to.fullPath.substr(2);
+//         next(path);
+//         return;
+//     }
+//     next();
+// });
