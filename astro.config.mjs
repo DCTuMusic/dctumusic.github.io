5ac809fb4315d273df5282a5d98200186c61546f
DCTuMusic <dctumusic@gmail.com>
2026-08-05 22:39:29 +0800

    DNS Change

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dctumusic.github.io',
-  base: '/DCTu_Site/',
+  // site: 'https://dctumusic.github.io',
+  site: 'https://dctumusic.com',
+  // base: '/DCTu_Site/',
+  base: '/',
   i18n: {
     locales: ['en', { path: 'zh-hant', codes: ['zh-Hant'] }],
     defaultLocale: 'en',
